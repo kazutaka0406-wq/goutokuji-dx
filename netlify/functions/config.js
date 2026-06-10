a32f6865444eb9faba351cf1c20820d084d606cf
kazutaka0406-wq <kazutaka0406@gmail.com>
2026-06-10 11:32:25 +0900

    STEP1：環境変数で課金モード切り替え実装

---
+++ netlify/functions/config.js
@@ -0,0 +1,12 @@
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({
+      paymentMode: process.env.PAYMENT_MODE || "demo"
+    })
+  };
+};
